7e9403034a4d230f4eed4001b4232c18a2c5ec31
Daniel Cho Codes <whgusrnr7755@gmail.com>
2022-02-16 18:35:23 +0900

    2022-02-16 18:34 Yarn And Json

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import{BrowserRouter as Router,Link,Route,Routes} from 'react-router-dom'
+import Home from "./hello/Home";
+import Bmi from "./hello/Bmi";
+import Calc from "./hello/Calc";
+import Change from "./hello/Change";
+import Circle from "./hello/Circle";
+import Grade from "./hello/Grade";
+import Login from "./hello/Login";
+import Week from "./hello/Week";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          리액트 배우기
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+return (
+        <div>
+          <Router>
+            <Routes>
+              <Route expected path="/" element={<Home/>}/>
+              <Route path="/bmi" element={<Bmi/>}/>
+              <Route path="/calc" element={<Calc/>}/>
+              <Route path="/change" element={<Change/>}/>
+              <Route path="/circle" element={<Circle/>}/>
+              <Route path="/grade" element={<Grade/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/week" element={<Week/>}/>
+            </Routes>
+          </Router>
+        </div>
+      )
 }
 
 export default App;
