e92de6d4d4ec9c7189aede9549a7a7cd7d802c4b
Daniel Cho Codes <whgusrnr7755@gmail.com>
2022-02-28 19:40:23 +0900

    2022-02-28 19:39 Module

--- containers/Layout.js
+++ containers/Layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Header from "./Header";
-import Footer from "./Footer";
+import { Header,Footer } from "./containers/index";
 
 export default function Layout( { children } ){
     return (<div>
